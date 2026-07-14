c358e158eb24f584c87d4fba3ad071252b55e8f4
8alubaid <faba4659@colorado.edu>
2026-07-14 16:13:52 +0300

    Navera CI/CD Test

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def run_command(command):
 def home():
     return jsonify({
         "message": "Network Diagnostic API is running",
+        "version": "Network Diagnostic API v2 deployed by Navera CI/CD",
         "endpoints": ["/ping?host=google.com", "/dns?domain=google.com", "/health"]
     })
 
