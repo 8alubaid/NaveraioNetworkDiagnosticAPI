90970fce9c2aecdd2519605d8ffee9b73a6f294e
8alubaid <faba4659@colorado.edu>
2026-07-14 16:59:54 +0300

    default

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ def run_command(command):
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Network Diagnostic API is running",
-        "version": "Network Diagnostic API v2 deployed by Navera CI/CD Faris did testing heer",
+        "message": "Network Diagnostic API is running", # Add ("version": "v2 deployed by Navera CI/CD")
         "endpoints": ["/ping?host=google.com", "/dns?domain=google.com", "/health"]
     })
 
